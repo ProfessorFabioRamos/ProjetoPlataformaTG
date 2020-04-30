ccdabba733b520bc8b8ce35b982d10446e40fd7e
Professor Fabio Ramos <fabio.sramos@ceub.edu.br>
2020-04-30 10:03:57 -0300

    Primeiro commit

---
+++ Projeto Plataforma TG/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public float movementSpeed = 5;
+    Rigidbody2D rig;
+    Animator anim;
+    SpriteRenderer spr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        rig.velocity = new Vector2(movementSpeed*h, rig.velocity.y);
+        anim.SetFloat("speed", Mathf.Abs(h));
+
+        if(h >0){
+            spr.flipX = false;
+        }
+        else if (h< 0){
+            spr.flipX = true;
+        }
+
+    }
+
+}
